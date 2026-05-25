b58789ab165cd74ddf7846d36864b994fe6484c0
Shehzadi Sundas <shehzadisundas306@gmail.com>
2026-05-25 15:56:58 +0500

    'update code'

--- server.js
+++ server.js
@@ -38,8 +38,17 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//     credentials: true,
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [
+        'http://localhost:3000',
+        'https://holistilearn-frontend.vercel.app/'
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
